21e5c320912ed705172966e9a7c97a5f6d353c67
Travis Ledo <travis.ledo@gmail.com>
2024-10-07 18:41:54 -0400

    Add https

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,13 @@ const booksRouter = require('./routes/booksRouter');
 const usersRouter = require('./routes/usersRouter');
 const witAiRouter = require('./routes/witAiRouter');
 const app = express();
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+};
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -15,9 +22,14 @@ app.use(cors());
 app.use(booksRouter);
 app.use(usersRouter);
 app.use(witAiRouter);
-app.listen(port, () => {
-  console.log(`App listening on port ${port}.`);
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`HTTPS Server running on port ${port}`);
 });
+
+//app.listen(port, () => {
+//  console.log(`App listening on port ${port}.`);
+//});
 
 mongoose
     .connect(process.env.DB)
