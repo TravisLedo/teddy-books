fc7e6e080890cc3fc1d141c6437c0be5df396c2a
Travis Ledo <travis.ledo@gmail.com>
2024-10-07 19:03:08 -0400

    Test revert https

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,6 @@ const booksRouter = require('./routes/booksRouter');
 const usersRouter = require('./routes/usersRouter');
 const witAiRouter = require('./routes/witAiRouter');
 const app = express();
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-};
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -22,14 +15,9 @@ app.use(cors());
 app.use(booksRouter);
 app.use(usersRouter);
 app.use(witAiRouter);
-
-https.createServer(options, app).listen(port, () => {
-  console.log(`HTTPS Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}.`);
 });
-
-//app.listen(port, () => {
-//  console.log(`App listening on port ${port}.`);
-//});
 
 mongoose
     .connect(process.env.DB)
